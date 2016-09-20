9bb6ab640cbe989ab866ac7aa26b61905dd671b1
znipps <znipps@gmail.com>
2016-09-20 15:33:47 +0800

    Site updated: 2016-09-20 15:33:46

---
+++ goodness/style.js
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="//cdn.bootcss.com/highlight.js/9.2.0/styles/github.min.css">
+<script src="//cdn.bootcss.com/highlight.js/9.2.0/highlight.min.js"></script>
+
+$(window).load(function(){
+ hljs.initHighlightingOnLoad();
+ })
